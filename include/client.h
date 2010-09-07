d3d0de3eef0a180be60b9506257e146127c3022e
Julien Thevenon <julien_thevenon@yahoo.fr>
2010-09-07 21:38:40 +0200

    Aligning class client on class derived from item class

--- include/client.h
+++ include/client.h
@@ -1,21 +1,137 @@
 #ifndef CLIENT_H
 #define CLIENT_H
 
-#include <stdint.h>
+#include "item.h"
 #include <string>
 
-class client
+class client: public item
 {
+
+  friend std::ostream& operator<<(std::ostream& s, const client & p_client);
+
  public:
-  client(uint32_t p_client_id,const std::string & p_surname, const std::string & p_first_name,const std::string & p_address,const std::string & p_tel, const std::string & p_postal_code, const std::string & p_town);
+  inline client(uint32_t p_id,const std::string & p_surname, const std::string & p_first_name,const std::string & p_address,const std::string & p_tel, uint32_t p_ville_id);
+  inline client(const std::string & p_surname, const std::string & p_first_name,const std::string & p_address,const std::string & p_tel, uint32_t p_ville_id);
+
+  inline const std::string & get_name(void)const;
+  inline void set_name(const std::string & p_name);
+  inline const std::string & get_first_name(void)const;
+  inline void set_first_name(const std::string & p_first_name);
+  inline const std::string & get_address(void)const;
+  inline void set_address(const std::string & p_address);
+  inline const std::string & get_tel(void)const;
+  inline void set_tel(const std::string & p_tel);
+  inline uint32_t get_ville_id(void)const;
+  inline void set_ville_id(uint32_t p_ville_id);
+
+  /**
+     Return type of object. This is used to name the corresponding table in database
+  */
+  inline const std::string getType(void)const;
  private:
-  uint32_t m_client_id;
   std::string m_surname;
   std::string m_first_name;
   std::string m_address;
   std::string m_tel;
-  std::string m_postal_code;
-  std::string m_town;
+  uint32_t m_ville_id;
 };
+
+inline std::ostream& operator<<(std::ostream& s, const client & p_item);
+
+//------------------------------------------------------------------------------
+const std::string client::getType(void)const
+{
+  return "Client";
+}
+
+//------------------------------------------------------------------------------
+client::client(uint32_t p_id,const std::string & p_surname, const std::string & p_first_name,const std::string & p_address,const std::string & p_tel, uint32_t p_ville_id):
+  item(p_id),
+  m_surname(p_surname),
+  m_first_name(p_first_name),
+  m_address(p_address),
+  m_tel(p_tel),
+  m_ville_id(p_ville_id)
+{
+}
+
+//------------------------------------------------------------------------------
+client::client(const std::string & p_surname, const std::string & p_first_name,const std::string & p_address,const std::string & p_tel, uint32_t p_ville_id):
+  item(),
+  m_surname(p_surname),
+  m_first_name(p_first_name),
+  m_address(p_address),
+  m_tel(p_tel),
+  m_ville_id(p_ville_id)
+{
+}
+
+//------------------------------------------------------------------------------
+const std::string & client::get_name(void)const
+{
+  return m_surname;
+}
+
+//------------------------------------------------------------------------------
+void client::set_name(const std::string & p_name)
+{
+  m_surname = p_name;
+}
+
+//------------------------------------------------------------------------------
+const std::string & client::get_first_name(void)const
+{
+  return m_first_name;
+}
+
+//------------------------------------------------------------------------------
+void client::set_first_name(const std::string & p_first_name)
+{
+  m_first_name = p_first_name;
+}
+
+//------------------------------------------------------------------------------
+const std::string & client::get_address(void)const
+{
+  return m_address;
+}
+
+//------------------------------------------------------------------------------
+void client::set_address(const std::string & p_address)
+{
+  m_address = p_address;
+}
+
+//------------------------------------------------------------------------------
+const std::string & client::get_tel(void)const
+{
+  return m_tel;
+}
+
+//------------------------------------------------------------------------------
+void client::set_tel(const std::string & p_tel)
+{
+  m_tel = p_tel;
+}
+
+//------------------------------------------------------------------------------
+uint32_t client::get_ville_id(void)const
+{
+  return m_ville_id;
+}
+
+//------------------------------------------------------------------------------
+void client::set_ville_id(uint32_t p_ville_id)
+{
+  m_ville_id = p_ville_id;
+}
+
+
+//------------------------------------------------------------------------------
+std::ostream& operator<<(std::ostream& s, const client & p_client)
+{
+  s << p_client.getType() << "{Id=" << p_client.getId() << "\",Name=\"" << p_client.m_surname << "\",First Name=\"" << p_client.m_first_name << "\",Tel=\"" << p_client.m_tel << "\",VilleId=\"" << p_client.m_ville_id << "\"" ;
+  return s;
+}
 
 #endif
---
+++ include/description_client.h
@@ -0,0 +1,106 @@
+#ifndef DESCRIPTION_CLIENT_H
+#define DESCRIPTION_CLIENT_H
+
+#include "description.h"
+#include "client.h"
+
+template <> class description<client>
+{
+
+ public:
+  inline static const std::string getClassType(void);
+  inline static const std::string getTableFieldsDeclaration(void);
+  inline static const std::string getTableFields(void);
+  inline static const std::string getUpdateFields(void);
+  inline static const std::string getFieldValues(const client & p_client);
+  inline static void bind_update_values(const client & p_client,sqlite3_stmt* p_stmt,sqlite3 *p_db);
+  inline static client* getItemFromRow(sqlite3_stmt* p_stmt);
+ private:
+  
+};
+
+
+//------------------------------------------------------------------------------
+const std::string description<client>::getClassType(void)
+{
+  return "Client";
+}
+
+//------------------------------------------------------------------------------
+const std::string description<client>::getTableFieldsDeclaration(void)
+{
+  return "Name TEXT, FirstName TEXT, Address TEXT, Tel TEXT, VilleId INTEGER";
+}
+
+//------------------------------------------------------------------------------
+const std::string description<client>::getTableFields(void)
+{
+  return "Name, FirstName, Address, Tel, VilleId";
+}
+
+//------------------------------------------------------------------------------
+const std::string description<client>::getUpdateFields(void)
+{
+  return "Name = $name, FirstName = $first_name, Address = $address , Tel = $tel , VilleId = $ville_id";
+}
+
+//------------------------------------------------------------------------------
+const std::string description<client>::getFieldValues(const client & p_client)
+{
+  std::stringstream l_result;
+  l_result << "\"" << p_client.get_name() << "\", \"" << p_client.get_first_name() << "\", \"" << p_client.get_address() <<  "\", \"" << p_client.get_tel() << "\", " << p_client.get_ville_id() ;
+  return l_result.str();
+}
+
+//------------------------------------------------------------------------------
+void description<client>::bind_update_values(const client & p_client,sqlite3_stmt* p_stmt,sqlite3 *p_db)
+{
+  int l_status = sqlite3_bind_text(p_stmt,sqlite3_bind_parameter_index(p_stmt,"$name"),p_client.get_name().c_str(),-1,SQLITE_STATIC);
+  if(l_status != SQLITE_OK)
+    {
+      std::cout << "ERROR during binding of name parameter for update statement of " << getClassType() << " : " << sqlite3_errmsg(p_db) << std::endl ;     
+      exit(-1);
+    }  
+
+  l_status = sqlite3_bind_text(p_stmt,sqlite3_bind_parameter_index(p_stmt,"$first_name"),p_client.get_first_name().c_str(),-1,SQLITE_STATIC);
+  if(l_status != SQLITE_OK)
+    {
+      std::cout << "ERROR during binding of first_name parameter for update statement of " << getClassType() << " : " << sqlite3_errmsg(p_db) << std::endl ;     
+      exit(-1);
+    }  
+
+  l_status = sqlite3_bind_text(p_stmt,sqlite3_bind_parameter_index(p_stmt,"$address"),p_client.get_address().c_str(),-1,SQLITE_STATIC);
+  if(l_status != SQLITE_OK)
+    {
+      std::cout << "ERROR during binding of address parameter for update statement of " << getClassType() << " : " << sqlite3_errmsg(p_db) << std::endl ;     
+      exit(-1);
+    }  
+
+  l_status = sqlite3_bind_text(p_stmt,sqlite3_bind_parameter_index(p_stmt,"$tel"),p_client.get_tel().c_str(),-1,SQLITE_STATIC);
+  if(l_status != SQLITE_OK)
+    {
+      std::cout << "ERROR during binding of tel parameter for update statement of " << getClassType() << " : " << sqlite3_errmsg(p_db) << std::endl ;     
+      exit(-1);
+    }  
+
+  l_status = sqlite3_bind_int(p_stmt,sqlite3_bind_parameter_index(p_stmt,"$ville_id"),p_client.get_ville_id());
+  if(l_status != SQLITE_OK)
+    {
+      std::cout << "ERROR during binding of ville_id parameter for update statement of " << getClassType() << " : " << sqlite3_errmsg(p_db) << std::endl ;     
+      exit(-1);
+    }  
+}
+
+//------------------------------------------------------------------------------
+client* description<client>::getItemFromRow(sqlite3_stmt* p_stmt)
+{
+  return new client(sqlite3_column_int(p_stmt,0),//Client Id
+		   (const char*)sqlite3_column_text(p_stmt,1),//Name
+		   (const char*)sqlite3_column_text(p_stmt,2),//First Name
+		   (const char*)sqlite3_column_text(p_stmt,3),//Address
+		   (const char*)sqlite3_column_text(p_stmt,4),//Tel
+		   sqlite3_column_int(p_stmt,5)//VilleId
+		   );
+}
+
+#endif
