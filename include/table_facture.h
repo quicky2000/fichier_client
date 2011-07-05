25cdea75b58c07579e1a7f8effeaa7c87fabfb1a
Julien Thevenon <julien_thevenon@yahoo.fr>
2011-07-05 02:39:15 +0200

    Adding request to get facture related to a status

--- include/table_facture.h
+++ include/table_facture.h
@@ -15,12 +15,14 @@ class table_facture: public base_table<facture>
   void get_by_date_and_client_id(const std::string & p_date,uint32_t p_client_id,std::vector<facture> & p_result);
   void get_by_livre_facture(uint32_t p_livre_id,std::vector<facture> & p_result);
   void get_by_livre_facture_and_ref(uint32_t p_facture_ref, uint32_t p_livre_facture_id,std::vector<facture> & p_result);
-
+  void get_by_status(uint32_t p_facture_status_id,std::vector<facture> & p_result);
+ 
  private:
   sqlite3_stmt *m_get_by_date_stmt;
   sqlite3_stmt *m_get_by_date_and_client_id_stmt;
   sqlite3_stmt *m_get_by_livre_facture_stmt;
   sqlite3_stmt *m_get_by_livre_facture_and_ref_stmt;
+  sqlite3_stmt *m_get_by_status_stmt;
 
 };
 
--- src/table_facture.cpp
+++ src/table_facture.cpp
@@ -8,13 +8,15 @@ table_facture::table_facture(void):
   m_get_by_date_stmt(NULL),
   m_get_by_date_and_client_id_stmt(NULL),
   m_get_by_livre_facture_stmt(NULL),
-  m_get_by_livre_facture_and_ref_stmt(NULL)
+  m_get_by_livre_facture_and_ref_stmt(NULL),
+  m_get_by_status_stmt(NULL)
 {
 }
 
 //------------------------------------------------------------------------------
 table_facture::~table_facture(void)
 {
+  sqlite3_finalize(m_get_by_status_stmt);
   sqlite3_finalize(m_get_by_livre_facture_and_ref_stmt);
   sqlite3_finalize(m_get_by_livre_facture_stmt);
   sqlite3_finalize(m_get_by_date_and_client_id_stmt);
@@ -29,7 +31,16 @@ void table_facture::set_db(sqlite3 *p_db)
 
   // Preparing get_by_date statements
   //--------------------------------------------
-  int l_status = sqlite3_prepare_v2(base_table<facture>::get_db(),("SELECT Id,"+description<facture>::getTableFields()+" FROM " + description<facture>::getClassType() + " WHERE Date LIKE @date").c_str(),-1,&m_get_by_date_stmt,NULL);
+  int l_status = sqlite3_prepare_v2(base_table<facture>::get_db(),
+				    ("SELECT Id," + 
+				     description<facture>::getTableFields() + 
+				     " FROM " + 
+				     description<facture>::getClassType() + 
+				     " WHERE Date LIKE @date"
+				     ).c_str(),
+				    -1,
+				    &m_get_by_date_stmt,
+				    NULL);
   if(l_status != SQLITE_OK)
     {
       std::cout << "ERROR during preparation of statement to get "+description<facture>::getTableFields()+" item by date : " << sqlite3_errmsg(base_table<facture>::get_db()) << std::endl ;     
@@ -38,7 +49,16 @@ void table_facture::set_db(sqlite3 *p_db)
 
   // Preparing livre facture get by date and client_id statements
   //--------------------------------------------
-  l_status = sqlite3_prepare_v2(base_table<facture>::get_db(),("SELECT Id,"+description<facture>::getTableFields()+" FROM " + description<facture>::getClassType() + " WHERE Date LIKE @date AND ClientId = $client_id").c_str(),-1,&m_get_by_date_and_client_id_stmt,NULL);
+  l_status = sqlite3_prepare_v2(base_table<facture>::get_db(),
+				("SELECT Id," + 
+				 description<facture>::getTableFields() + 
+				 " FROM " + 
+				 description<facture>::getClassType() + 
+				 " WHERE Date LIKE @date AND ClientId = $client_id"
+				 ).c_str(),
+				-1,
+				&m_get_by_date_and_client_id_stmt,
+				NULL);
   if(l_status != SQLITE_OK)
     {
       cout << "ERROR during preparation of statement to get " << description<facture>::getClassType() << " item by date and client id : " << sqlite3_errmsg(base_table<facture>::get_db()) << endl ;     
@@ -68,10 +88,40 @@ void table_facture::set_db(sqlite3 *p_db)
 
   // Preparing livre facture get by ref and livre facture id statements
   //--------------------------------------------
-  l_status = sqlite3_prepare_v2(base_table<facture>::get_db(),("SELECT Id,"+description<facture>::getTableFields()+" FROM " + description<facture>::getClassType() + " WHERE FactureRef = $facture_ref AND LivreFactureId = $livre_facture_id").c_str(),-1,&m_get_by_livre_facture_and_ref_stmt,NULL);
+  l_status = sqlite3_prepare_v2(base_table<facture>::get_db(),
+				("SELECT Id," + 
+				 description<facture>::getTableFields() + 
+				 " FROM " + 
+				 description<facture>::getClassType() + 
+				 " WHERE FactureRef = $facture_ref AND LivreFactureId = $livre_facture_id"
+				 ).c_str(),
+				-1,
+				&m_get_by_livre_facture_and_ref_stmt,
+				NULL);
   if(l_status != SQLITE_OK)
     {
       cout << "ERROR during preparation of statement to get " << description<facture>::getClassType() << " item by livre id and facture ref : " << sqlite3_errmsg(base_table<facture>::get_db()) << endl ;     
+      exit(-1);
+    }
+
+  // Preparing livre facture get by status statements
+  //-------------------------------------------------------------
+  l_status = sqlite3_prepare_v2(base_table<facture>::get_db(),
+				("SELECT Id," + 
+				 description<facture>::getTableFields() + 
+				 " FROM " + 
+				 description<facture>::getClassType() + 
+				 " WHERE " +
+				 "Status = $status" + 
+				 " ORDER BY " + 
+				 description<facture>::getClassType() + ".FactureRef"
+				 ).c_str(),
+				-1,
+				&m_get_by_status_stmt,
+				NULL);
+  if(l_status != SQLITE_OK)
+    {
+      cout << "ERROR during preparation of statement to get " << description<facture>::getClassType() << " item by status : " << sqlite3_errmsg(base_table<facture>::get_db()) << endl ;     
       exit(-1);
     }
 }
@@ -281,6 +331,53 @@ void table_facture::get_by_livre_facture_and_ref(uint32_t p_facture_ref, uint32_
   if(l_status != SQLITE_OK)
     {
       cout << "ERROR during reset of bindings of " << description<facture>::getClassType() << " get_by_livre_facture statement : " << sqlite3_errmsg(base_table<facture>::get_db()) << endl ;     
+      exit(-1);
+    }
+
+}
+
+//------------------------------------------------------------------------------
+void table_facture::get_by_status(uint32_t p_facture_status_id,std::vector<facture> & p_result)
+{
+  // Binding values to statement
+  //----------------------------
+  int l_status = sqlite3_bind_int(m_get_by_status_stmt,sqlite3_bind_parameter_index(m_get_by_status_stmt,"$status"),p_facture_status_id);
+  if(l_status != SQLITE_OK)
+    {
+      cout << "ERROR during binding of status parameter for get_by_facture_status of " << description<facture>::getClassType() << " : " << sqlite3_errmsg(base_table<facture>::get_db()) << endl ;     
+      exit(-1);
+    }
+    
+  // Executing statement
+  //---------------------
+  while( (l_status = sqlite3_step(m_get_by_status_stmt)) == SQLITE_ROW)
+    {
+      p_result.push_back(description<facture>::getItemFromRow(m_get_by_status_stmt));
+    }
+  if(l_status != SQLITE_DONE)
+    {
+      cout << "ERROR during selection of " << description<facture>::getClassType() << " : " << sqlite3_errmsg(base_table<facture>::get_db()) << endl ;
+      exit(-1);
+    }
+
+  cout << description<facture>::getClassType() << " get_by_facture_status successfully listed" << endl ;
+
+
+  // Reset the statement for the next use
+  //--------------------------------------
+  l_status = sqlite3_reset(m_get_by_status_stmt);  
+  if(l_status != SQLITE_OK)
+    {
+      cout << "ERROR during reset of " << description<facture>::getClassType() << " containing_date statement : " << sqlite3_errmsg(base_table<facture>::get_db()) << endl ;     
+      exit(-1);
+    }
+
+  // Reset bindings because they are now useless
+  //--------------------------------------------
+  l_status = sqlite3_clear_bindings(m_get_by_status_stmt);
+  if(l_status != SQLITE_OK)
+    {
+      cout << "ERROR during reset of bindings of " << description<facture>::getClassType() << " containing_date statement : " << sqlite3_errmsg(base_table<facture>::get_db()) << endl ;     
       exit(-1);
     }
 
